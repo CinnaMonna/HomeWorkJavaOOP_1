057857617419984ec7d2a22f5c98274b92cf3e0e
Natalia <nivanova86@bk.ru>
2023-04-10 01:44:41 +0300

    complete task1 SOLID

--- HW6_1/solid-principle-first-seminar_1/src/homework/Main.java
+++ HW6_1/solid-principle-first-seminar_1/src/homework/Main.java
@@ -1,7 +1,7 @@
 package homework;
 
-public class Main{
-    public static void main(String[] args){
+public class Main {
+    public static void main(String[] args) {
         User user = new User("Bob");
         user.report();
         user.save();
